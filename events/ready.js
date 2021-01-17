bd4a53922197bfb0fd96605db45d2eda2deec65c
Glitch (invite-athanasia) <none>
2021-01-17 15:54:47 +0000

    🏇⛳️ Checkpoint ./events/ready.js:27372622/642

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,17 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
+  console.log(`[BOT] | (${client.user.username}) HAZIR KAPTAN STRIGA !
+  ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
+  client.user.setStatus("dnd");
+  client.user.setActivity("BERAT ❤️ ARTİUS", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+  console.log(`ABILERE SELAM STRIGAYLA DEWAM`);
+
+};
 
