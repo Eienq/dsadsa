05efec56ed695fa4d92ea5221cb910f925f722ad
Glitch (invite-athanasia) <none>
2021-01-17 15:42:51 +0000

    ⛑🐣 Checkpoint ./ayarlar.json:27372622/41 ./events/ready.js:27372622/1312

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,1 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
-module.exports = client => {
-  console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
-  console.log(`[BOT] | (${client.user.username}) HAZIR KAPTAN STRIGA !
-  ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
-  client.user.setStatus("online");
-  client.user.setActivity("BERAT ❤️ ARTİUS", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
-    if (client.channels.cache.has(ayarlar.voicechannel)) client.channels.cache.get(ayarlar.voicechannel).join().catch();
-  console.log(`LOG İNVİTES`);
-};
+
